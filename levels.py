54366cf00e7753a269e58491fadbdf8410dd05a5
Ivar van den Bosch <ivvdbosch@gmail.com>
2024-12-15 22:21:54 +0100

    Te veel

--- levels.py
+++ levels.py
@@ -2,12 +2,25 @@ import librosa
 import pygame
 
 source = 'nummers/apt.mp3'
+bpmArray = {'nummers/notlikeus.mp3': 101, 'nummers/apt.mp3': 149, 'nummers/prayer.mp3': 123, 'nummers/testSong.mp3': 97,'nummers/100bpm.mp3': 100}
+bpm = bpmArray[source]
+
+
+
+
 
 x, sr = librosa.load(source)
 
-tempo, beat_times = librosa.beat.beat_track(y=x, sr=sr, start_bpm=149, units='time')
+tempo, beat_times = librosa.beat.beat_track(y=x, sr=sr, start_bpm=bpm, units='time')
 
 def playMusic() :
+  bpm = bpmArray[source]
   pygame.mixer.music.load(source)
-  pygame.mixer.music.set_volume(0.7)
-  pygame.mixer.music.play(-1) 
+  pygame.mixer.music.set_volume(0.4)
+  pygame.mixer.music.play(0) 
+
+def pauseMusic():
+  pygame.mixer.music.pause()
+
+def resumeMusic():
+  pygame.mixer.music.unpause()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import arrow
 import levels
 import time
+import os
 
 pygame.init()
 
@@ -22,6 +23,7 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
+
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Izza's Rhythm Spel")
@@ -42,79 +44,179 @@ def handle_input(event, pressed_keys):
         if event.key in pressed_keys:
             pressed_keys.remove(event.key)
 
-
-def process_hits(arrows, pressed_keys, score):
+def process_hits(arrows, pressed_keys, score, lineColor, lastHitTime, currentImage, images):
     for key in pressed_keys:
         if key in LANE_KEY_MAP:
             lane, rotation = LANE_KEY_MAP[key]
-            for thisArrow in arrows[:]:
-                if (HIT_LINE_Y - ARROW_OUTLINE_HEIGHT < thisArrow.y <
-                        HIT_LINE_Y + 50 and thisArrow.rotation == rotation):
-                    arrows.remove(thisArrow)
-                    score += 100
-                    break
-    return score
 
+        for thisArrow in arrows[:]:
+            if (HIT_LINE_Y - ARROW_OUTLINE_HEIGHT < thisArrow.y < HIT_LINE_Y + 50
+            and thisArrow.rotation == rotation):
+                arrows.remove(thisArrow)
+                score += 100
+                lineColor = GREEN
+                currentImage = images[lane]
+                lastHitTime = time.time()
+                break
+    return score, lineColor, lastHitTime,currentImage
 
 def main():
     running = True
+    playing = False
     clock = pygame.time.Clock()
     arrows = []
     spawn_timer = 0
     score = 0
     pressed_keys = []
-
+    won = False
+    lineColor = RED
+    lastHitTime = 0 
     ArrowClass = arrow.Arrow
+    currentImage = None
 
-    time.sleep(10)
+    images = [
+        pygame.image.load(os.path.join('dans-fotos', 'Left-blue.png')),
+        pygame.image.load(os.path.join('dans-fotos', 'Down-blue.png')),
+        pygame.image.load(os.path.join('dans-fotos', 'Up-blue.png')),
+        pygame.image.load(os.path.join('dans-fotos', 'Right-blue.png'))
+    ]
     
-    levels.playMusic()
+
+    numArray = [pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9]
     
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                
             handle_input(event, pressed_keys)
+            if event.type == pygame.KEYDOWN:
+                if event.key in numArray:
+                    number = numArray.index(event.key) -1 
+                    if (number > len( os.listdir('./nummers')) -1 ):
+                        number = 0
+                        
+                    selectedSong = 'nummers/' + os.listdir('./nummers')[number]
+                    levels.bpm = levels.bpmArray[selectedSong]
+                    levels.source = selectedSong
+                if event.key == pygame.K_SPACE and won:
+                    playing = True
+                    won = False
+                    spawn_timer = 0
+                    score = 0
+                    arrows = []
+                    pressed_keys = []
+                    ArrowClass = arrow.Arrow
+                    levels.playMusic()
+                if event.key == pygame.K_SPACE and not playing:
+                    playing = True
+                    won = False
+                    spawn_timer = 0
+                    score = 0
+                    arrows = []
+                    pressed_keys = []  
+                    ArrowClass = arrow.Arrow
+                    levels.playMusic()
+                if event.key == pygame.K_SPACE and won:
+                    playing = True
+                    won = False
+                    spawn_timer = 0
+                    score = 0
+                    arrows = []
+                    pressed_keys = []  
+                    ArrowClass = arrow.Arrow
+                    levels.playMusic()
+                
 
-        spawn_timer += clock.get_time()
-        tolerance = 0.01 
-        dropTime = 1430 # Het duurt 1.43 sec om te vallen
-        for sec in levels.beat_times:
-            if abs((spawn_timer + dropTime) / 1000 - sec) < tolerance:
-                random.seed(sec) # zorg ervoor dat sequentie van willekeur altijd hetzelfde is bij ieder nummer
-                lane_index = random.randint(0, LANE_COUNT - 1)
-                rotation = [90, 180, 0, -90][lane_index]
-                arrows.append(
-                    ArrowClass(
-                        screen,
-                        x=lane_index * LANE_WIDTH + LANE_WIDTH // 2 - 25,
-                        y=0,
-                        width=50,
-                        height=50,
-                        gravity=GLOBAL_GRAVITY,
-                        rotation=rotation,
-                    ))
+        if not playing:
+            screen.fill(BLACK)
+            text = font.render("Press SPACE to start", True, WHITE)
+            nummers = os.listdir('./nummers')
+            screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+            for i in range(len(nummers)):
+                if ("nummers/"+nummers[i]) == levels.source:
+                    text = font.render(f"{i+1}. {nummers[i]}", True, GREEN)
+                else: 
+                    text = font.render(f"{i+1}. {nummers[i]}", True, WHITE)
+                screen.blit(text, (WIDTH // 2 - text.get_width() // 2, 100 +  HEIGHT // 2 + ((text.get_height() - 100) * (-i+1))// 2))
+        
+              
+                
+        if playing:
+            spawn_timer += clock.get_time()
+            tolerance = 0.01 
+            dropTime = 1430 # Het duurt 1.34 sec om te vallen
+            songEndBuffer = 3 
+            for sec in levels.beat_times:
+                if abs((spawn_timer + dropTime) / 1000 - sec) < tolerance:
+                    random.seed(sec) # zorg ervoor dat sequentie van willekeur altijd hetzelfde is bij ieder nummera
+                    lane_index = random.randint(0, LANE_COUNT - 1)
+                    rotation = [90, 180, 0, -90][lane_index]
+                    arrows.append(
+                        ArrowClass(
+                            screen,
+                            x=lane_index * LANE_WIDTH + LANE_WIDTH // 2 - 25,
+                            y=0,
+                            width=50,
+                            height=50,
+                            gravity=GLOBAL_GRAVITY,
+                            rotation=rotation,
+                        ))
+                if levels.beat_times[-1] + songEndBuffer < (spawn_timer + dropTime) / 1000:
+                    won = True
+                    break
+           
 
-        for thisArrow in arrows[:]:
-            thisArrow.update()
-            if thisArrow.y > HIT_LINE_Y + ARROW_OUTLINE_HEIGHT:
-                arrows.remove(thisArrow)
-                score -= 50
+            for thisArrow in arrows[:]:
+                thisArrow.update()
+                if thisArrow.y > HIT_LINE_Y + ARROW_OUTLINE_HEIGHT:
+                    arrows.remove(thisArrow)
+                    if score - 50 > 0:
+                        score -= 50
+                
+            if not won:
+                score, lineColor, lastHitTime, currentImage = process_hits(arrows, pressed_keys, score, lineColor, lastHitTime, currentImage, images)
 
-        score = process_hits(arrows, pressed_keys, score)
+                if time.time() - lastHitTime >= 0.3:
+                    lineColor = RED 
 
-        screen.fill(WHITE)
-        for thisArrow in arrows:
-            thisArrow.draw()
+                screen.fill(WHITE)
+                
+                if currentImage is not None:
+                    maxWidth = WIDTH // 2
+                    maxHeight = HEIGHT // 2
 
-        pygame.draw.line(screen, RED, (0, HIT_LINE_Y), (WIDTH, HIT_LINE_Y), 2)
+                    scaleFactor = min(maxWidth / currentImage.get_width(), maxHeight / currentImage.get_height())
 
-        score_text = font.render(f"Score: {score}", True, BLACK)
-        screen.blit(score_text, (10, 10))
+                    newWidth = int(currentImage.get_width() * scaleFactor)
+                    newHeight = int(currentImage.get_height() * scaleFactor)
+
+                    scaledImage = pygame.transform.smoothscale(currentImage, (newWidth, newHeight))
+
+                    # Blit the scaled image to the center of the screen
+                    screen.blit(scaledImage, (WIDTH // 2 - newWidth // 2, HEIGHT // 2 - newHeight // 2))
+
+                for thisArrow in arrows:
+                    thisArrow.draw()
+                pygame.draw.line(screen, lineColor, (0, HIT_LINE_Y), (WIDTH, HIT_LINE_Y), 2)
+                score_text = font.render(f"Score: {score}", True, BLACK)
+
+               
+
+                screen.blit(score_text, (10, 10))
+            
+            if won: 
+                screen.fill(BLACK)
+                text = font.render(f"Your score: {score}", True, WHITE)
+                screen.blit(text, (WIDTH // 2 - text.get_width() // 2, (HEIGHT // 2 + text.get_height() // 2) + 100))
+                text = font.render(f"You scored: {round((score)/(len(levels.beat_times)), 1)}%", True, WHITE)
+                screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 + text.get_height() // 2))
+                text = font.render("Press SPACE to play again", True, WHITE)
+                screen.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
+           
 
         pygame.display.flip()
         clock.tick(60)
-
     pygame.mixer.quit()
     pygame.quit()
     sys.exit()
